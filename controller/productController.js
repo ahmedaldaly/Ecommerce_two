ba1c826d109844872a1fd5675109b260a82140af
ahmedaldaly <ranaandahmed55@gmail.com>
2025-04-21 10:56:17 +0200

    delete and edit product

--- controller/productController.js
+++ controller/productController.js
@@ -148,4 +148,18 @@ module.exports.deleteProductImage = asyncHandler(async (req, res) => {
       res.status(500).json({ message: 'Server error', error: err });
     }
   });
+  module.exports.getProductByCategory = asyncHandler(async (req, res) => {
+    const category = req.query.category;
   
+    if (!category) {
+      return res.status(400).json({ message: "Category is required" });
+    }
+  
+    const products = await Product.find({ Category: category });
+  
+    if (products.length === 0) {
+      return res.status(404).json({ message: "No products found for this category" });
+    }
+  
+    res.status(200).json(products);
+  });
--- router/product.js
+++ router/product.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const multer = require ('multer');
 const upload = multer ({dest:'uploads/'})
-const {addProduct,getAllProducts,deleteProduct,getProduct ,deleteProductImage,editProduct} = require('../controller/productController')
+const {addProduct,getAllProducts,deleteProduct,getProduct ,deleteProductImage,editProduct,getProductByCategory} = require('../controller/productController')
 router.route('/').post(upload.array('image'),addProduct).get(getAllProducts).delete(deleteProductImage)
+router.route('/by').get(getProductByCategory)
 router.route('/:id').delete(deleteProduct).get(getProduct).put(upload.array('image'),editProduct)
 module.exports = router;
