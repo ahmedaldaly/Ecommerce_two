1ca2f4326c35050edbcc43d4eaec4048450e5aae
ahmedaldaly <ranaandahmed55@gmail.com>
2025-04-21 07:25:19 +0200

    add product

--- controller/productController.js
+++ controller/productController.js
@@ -14,36 +14,38 @@ module.exports.addProduct = asyncHandler (async (req, res)=>{
     try{
         const findCategory = await Category.find({name:req.body.category});
         const findBrand = await Brand.find({name:req.body.brand});
-        if (!findCategory || !findBrand) {
+        const check = await Product.find({title:req.body.title , brand:req.body.brand})
+        if (!findCategory || !findBrand|| check) {
             res.status(404).json({message:'category or brand not found'})
+        }else{
+            const images = [];
+            for (const file of req.files) {
+              const result = await cloudinary.uploader.upload(file.path);
+              images.push({
+                url: result.secure_url,
+                id: result.public_id,
+              });
+            }
+            const newProduct = new Product ({
+                title:req.body.title,
+                description:req.body.description,
+                price:req.body.price,
+                price:req.body.price,
+                Category:req.body.Category,
+                brand:req.body.brand,
+                quantity:req.body.quantity,
+                color:req.body.color,
+                size:req.body.size,
+                discount:req.body.discount,
+                image: images,
+            })
+            
+            const save = await newProduct.save();
+            for (const file of req.files) {
+               fs.unlinkSync(file.path)
+              }
+            res.status(201).json(save);
         }
-        const images = [];
-        for (const file of req.files) {
-          const result = await cloudinary.uploader.upload(file.path);
-          images.push({
-            url: result.secure_url,
-            id: result.public_id,
-          });
-        }
-        const newProduct = new Product ({
-            title:req.body.title,
-            description:req.body.description,
-            price:req.body.price,
-            price:req.body.price,
-            Category:req.body.Category,
-            brand:req.body.brand,
-            quantity:req.body.quantity,
-            color:req.body.color,
-            size:req.body.size,
-            discount:req.body.discount,
-            image: images,
-        })
-        
-        const save = await newProduct.save();
-        for (const file of req.files) {
-           fs.unlinkSync(file.path)
-          }
-        res.status(201).json(save);
     }catch(err){
         for (const file of req.files) {
             fs.unlinkSync(file.path)
--- router/product.js
+++ router/product.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const multer = require ('multer');
 const upload = multer ({dest:'uploads/'})
-const {addProduct} = require('../controller/productController')
-router.route('/').post(upload.array('image'),addProduct)
+const {addProduct,getAllProducts} = require('../controller/productController')
+router.route('/').post(upload.array('image'),addProduct).get(getAllProducts)
 module.exports = router;
