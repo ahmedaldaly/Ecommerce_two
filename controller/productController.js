fa70c7cf0345bd85b2d01bc0739b5982eac6a0e8
ahmedaldaly <ranaandahmed55@gmail.com>
2025-04-21 07:37:59 +0200

    add product

--- controller/productController.js
+++ controller/productController.js
@@ -51,4 +51,16 @@ module.exports.addProduct = asyncHandler (async (req, res)=>{
             fs.unlinkSync(file.path)
            }
         res,status(500).json(err)}
+});
+module.exports.deleteProduct = asyncHandler (async (req, res)=>{
+    try{
+        const find = await Product.findById(req.params.id);
+        if (!find)res.status(404).json({message:'product not found'});
+        const image = find.image ;
+        for (const img of image){
+            const remove = await cloudinary.uploader.destroy(img.id);
+        }
+        const remove = await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:'product deleted success'})
+    }catch(err){res.status(500).json(err)}
 })
--- router/product.js
+++ router/product.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const multer = require ('multer');
 const upload = multer ({dest:'uploads/'})
-const {addProduct,getAllProducts} = require('../controller/productController')
+const {addProduct,getAllProducts,deleteProduct} = require('../controller/productController')
 router.route('/').post(upload.array('image'),addProduct).get(getAllProducts)
+router.route('/:id').delete(deleteProduct)
 module.exports = router;
